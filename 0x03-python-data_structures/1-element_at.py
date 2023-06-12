65c32a1a43c2b32e535086af95fa9ce97173cfa0
Mary Mtetwa <kutaudziramary123@gmail.com>
2023-06-12 12:26:50 -0700

    i element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def main():
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
-if __name__ == "__main__":
-    main()
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    elif idx > len(my_list) - 1:
+        return (None)
+    else:
+        return (my_list[idx])
